97b859aaa64eb6adb9b471eb2aae3e5990d4d37b
ABDULSATTAR <ABDULSATTAR@IT-98>
2020-12-29 09:34:44 +0500

    AS - (Add FilePath removed by Arslan) API By Abdul Sattar

--- FOX.DataModels/Models/CommonModel/Common.cs
+++ FOX.DataModels/Models/CommonModel/Common.cs
@@ -69,6 +69,7 @@ namespace FOX.DataModels.Models.CommonModel
         public bool Success { get; set; }
         public string ID { get; set; }
         public bool AU { get; set; }
+        public string FilePath { get; set; }
     }
     public class SmartSearch
     {
