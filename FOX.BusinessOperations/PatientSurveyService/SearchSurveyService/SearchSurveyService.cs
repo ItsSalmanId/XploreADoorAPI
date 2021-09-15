d8ccac8523b6669c4b914bcf95adbc16382c2211
muhammadarslan3 <muhammadarslan3@IT-98>
2021-09-15 11:54:58 +0500

    MA3 - Item P450 | Surveys for Deceased & Not Interested Patients - API By Muhammad Arslan

--- FOX.BusinessOperations/PatientSurveyService/SearchSurveyService/SearchSurveyService.cs
+++ FOX.BusinessOperations/PatientSurveyService/SearchSurveyService/SearchSurveyService.cs
@@ -1,6 +1,7 @@
 ﻿using FOX.BusinessOperations.CommonService;
 using FOX.DataModels.Context;
 using FOX.DataModels.GenericRepository;
+using FOX.DataModels.Models.CommonModel;
 using FOX.DataModels.Models.PatientSurvey;
 using FOX.DataModels.Models.Security;
 using System;
@@ -39,7 +40,53 @@ namespace FOX.BusinessOperations.PatientSurveyService.SearchSurveyService
             var _dateTo = Helper.getDBNullOrValue("DATE_TO",dateTo.ToString());
             var PatientSurveyList = SpRepository<PatientSurvey>.GetSingleObjectWithStoreProcedure(@"exec FOX_PROC_GET_RANDOM_PATIENT_SURVEY @PRACTICE_CODE, @DATE_FROM, @DATE_TO", 
                 PracticeCode, _dateFrom, _dateTo);
+            CheckDeceasedPatient(PatientSurveyList.PATIENT_ACCOUNT_NUMBER.ToString(), practiceCode);
             return PatientSurveyList;
+        }
+        public ResponseModel CheckDeceasedPatient(string patientAccount, long practiceCode)
+        {
+            ResponseModel responseModel = new ResponseModel();
+            var result = _patientSurveyRepository.GetMany(x => x.PATIENT_ACCOUNT_NUMBER.ToString() == patientAccount && x.DELETED != true && x.PRACTICE_CODE == practiceCode);
+            if (result != null && result.Count > 0)
+            {
+                foreach (var item in result)
+                {
+                    if (item.SURVEY_STATUS_CHILD != null && item.SURVEY_STATUS_CHILD.ToLower().Contains("deceased"))
+                    {
+                        UpdatePatientSurvey(result);
+                        responseModel.Message = "SUccessfully Updated";
+                        responseModel.Success = true;
+                        responseModel.ErrorMessage = "";
+                    }
+                    else
+                    {
+                        responseModel.Message = "Not Updated";
+                        responseModel.Success = false;
+                        responseModel.ErrorMessage = "";
+                    }
+                }
+            }
+            else
+            {
+                responseModel.Message = "Response is Empty";
+                responseModel.Success = false;
+                responseModel.ErrorMessage = "";
+            }
+            return responseModel;
+        }
+        public void UpdatePatientSurvey(List<PatientSurvey> patientSurveys)
+        {
+            if (patientSurveys != null)
+            {
+                foreach (var item in patientSurveys)
+                {
+                    item.DELETED = true;
+                    item.MODIFIED_DATE = DateTime.Now;
+                    item.FEEDBACK = "Patient marked as deceased as of " + DateTime.Now;
+                    _patientSurveyRepository.Update(item);
+                    _patientSurveyRepository.Save();
+                }
+            }
         }
     }
 }
