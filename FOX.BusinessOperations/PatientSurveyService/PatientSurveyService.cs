b2c8ef5fc5de3f567280eccae0f4fd3a0cdd93fd
AFTAB KHAN <AFTABKHAN@carecloud.com>
2022-06-29 18:41:28 +0500

    Revert "FOX-6576 - Map incoming calls with patient survey   API BY AFTAB KHAN"
    This reverts commit 5bf0f5d0737928b7191df86658528af70a8bc301.

--- FOX.BusinessOperations/PatientSurveyService/PatientSurveyService.cs
+++ FOX.BusinessOperations/PatientSurveyService/PatientSurveyService.cs
@@ -781,19 +781,7 @@ namespace FOX.BusinessOperations.PatientSurveyService
                     data["Extension"] = patientSurveyCall.Extension;
                     data["FileName"] = patientSurveyCall.FileName;
                     // data["TeamID"] = AppConfiguration.ACUServiceTeamId;
-
-                    if(data["Extension"] == "8381")
-                    {
-                        data["TeamID"] = AppConfiguration.ACUServiceTeamId2;
-                    }
-                    if (data["Extension"] == "8384")
-                    {
-                        data["TeamID"] = AppConfiguration.ACUServiceTeamId3;
-                    }
-                    if(data["Extension"] != "8384" && data["Extension"] != "8381")
-                    {
-                        data["TeamID"] = AppConfiguration.ACUServiceTeamId;
-                    }
+                    data["TeamID"] = AppConfiguration.ACUServiceTeamId;
                     //data["Number"] = "7328735133";
                     //data["Extension"] = "1111";
                     //data["FileName"] = patientSurveyCall.FileName;
