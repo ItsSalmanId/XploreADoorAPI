d1c9c7a70fc7c0abc5d8948ea7064aac5d86761c
muhammadarslan3 <muhammadarslan3@OP2R4-4>
2022-12-06 19:05:57 +0500

    MA3 - Remove Redundant code from Test Cases - API By Muhammad Arslan Tufail.

--- FoxRehabilitation.UnitTest/DashboardServiceUnitTest/DashoboardServiceUnitTest.cs
+++ FoxRehabilitation.UnitTest/DashboardServiceUnitTest/DashoboardServiceUnitTest.cs
@@ -1,15 +1,10 @@
 ﻿using FOX.BusinessOperations.DashboardServices;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FoxRehabilitation.UnitTest.DashboardServiceUnitTest
 {
     [TestFixture]
-    class DashoboardServiceUnitTest
+    public class DashoboardServiceUnitTest
     {
         private DashboardService _dashboardServiceObj;
 
@@ -17,13 +12,9 @@ namespace FoxRehabilitation.UnitTest.DashboardServiceUnitTest
         public void Setup()
         {
             _dashboardServiceObj = new DashboardService();
-            
         }
+
         [Test]
-        [TestCase("")]
-        [TestCase(null)]
-        [TestCase(-1)]
-        [TestCase(101116333223)]
         [TestCase(1011163)]
         public void GetDashboardGetTotal_NullEmptyOutOfRangeParameter_DataOrErrorMsg(long practiceCode)
         {
@@ -38,17 +29,8 @@ namespace FoxRehabilitation.UnitTest.DashboardServiceUnitTest
             {
                 Assert.IsTrue(true);
             }
-            else
-            {
-                Assert.IsFalse(false);
-            }
         }
-
-
         [Test]
-        [TestCase("","","")]
-        [TestCase(null,null,null)]
-        [TestCase(-1, -1, -1)]
         [TestCase(01, 25, 1011163)]
         public void GetDashboardData_NullEmptyOutOfRangeParameter_DataOrErrorMsg(int value, int hourFrom, long practiceCode)
         {
@@ -56,29 +38,20 @@ namespace FoxRehabilitation.UnitTest.DashboardServiceUnitTest
             var PracticeCode = practiceCode;
 
             //Act
-            var result = _dashboardServiceObj.GetDashboardData(value,hourFrom, PracticeCode);
+            var result = _dashboardServiceObj.GetDashboardData(value, hourFrom, PracticeCode);
 
             //Assert
             if (result != null)
             {
                 Assert.IsTrue(true);
             }
-            else
-            {
-                Assert.IsFalse(false);
-            }
         }
-
-
         [Test]
-        [TestCase("","","","")]
-        [TestCase(null, null, null,null)]
-        [TestCase(-1, -1, "","")]
+        [TestCase(null, null, null, null)]
+        [TestCase(-1, 1011163, "", "")]
         public void GetDashboardTrend_NullEmptyOutOfRangeParameter_DataOrErrorMsg(int value, long practiceCode, string dateFromUser, string dateToUser)
         {
             //Arrange
-            var PracticeCode = practiceCode;
-
             //Act
             var result = _dashboardServiceObj.GetDashboardTrend(value, practiceCode, dateFromUser, dateToUser);
 
@@ -87,22 +60,13 @@ namespace FoxRehabilitation.UnitTest.DashboardServiceUnitTest
             {
                 Assert.IsTrue(true);
             }
-            else
-            {
-                Assert.IsFalse(false);
-            }
         }
-
-
         [Test]
-        [TestCase("", "", "", "","")]
-        [TestCase(null, null, null, null,null)]
-        [TestCase(-1,"", "",-1,25)]
+        [TestCase(null, null, null, null, null)]
+        [TestCase(1011163, "", "", 1, 25)]
         public void GetNoOfRecordBytime_NullEmptyOutOfRangeParameter_DataOrErrorMsg(long practiceCode, string dateFrom, string dateTo, int hourFrom, int hourTo)
         {
             //Arrange
-            var PracticeCode = practiceCode;
-
             //Act
             var result = _dashboardServiceObj.GetNoOfRecordBytime(practiceCode, dateFrom, dateTo, hourFrom, hourTo);
 
@@ -111,18 +75,12 @@ namespace FoxRehabilitation.UnitTest.DashboardServiceUnitTest
             {
                 Assert.IsTrue(true);
             }
-            else
-            {
-                Assert.IsFalse(false);
-            }
         }
-
         [TearDown]
         public void Teardown()
         {
             // Optionally dispose or cleanup objects
             _dashboardServiceObj = null;
-          
         }
     }
 }
