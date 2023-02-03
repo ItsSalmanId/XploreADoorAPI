aa29c9673786c5a0e2559554600a399d901af135
MUHAMMAD SALMAN <MUHAMMADSALMAN7@carecloud.com>
2023-02-03 15:00:59 +0500

    MS7 - Perform survey Sp Fixation - DB Schema By muhammad Salman

---
+++ Fox.Database/StoredProcedure/FOX_PROC_EXISTING_SURVEY_DETAIL.sql
@@ -0,0 +1,20 @@
+-- =============================================                    
+-- Created By :  Muhammad Salman                    
+-- Created date: 02/03/2023                    
+-- =============================================      
+CREATE PROCEDURE [DBO].[FOX_PROC_EXISTING_SURVEY_DETAIL]   
+ (@PRACTICE_CODE   BIGINT,                                                                                                                                                                   
+  @SURVEY_ID   BIGINT                                                                                   
+ )                                                                                                      
+ AS                                                                                                              
+   BEGIN    
+Select * FROm FOX_TBL_PATIENT_SURVEY PS  
+left JOIN FOX_TBL_SURVEY_AUTOMATION_SERVICE_LOG SL WITH (NOLOCK) ON PS.SURVEY_ID = SL.SURVEY_ID   
+where SL.SURVEY_ID = @SURVEY_ID    
+AND  
+SL.SURVEY_ID = @SURVEY_ID   
+AND ISNULL(SL.DELETED, 0) = 0     
+AND ISNULL(PS.DELETED, 0) = 0     
+AND IS_SURVEYED = 1  
+and PS.PRACTICE_CODE = @PRACTICE_CODE  
+end
