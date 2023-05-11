e55773384c27a3d31b230e96ce048cbb28f37855
MUHAMMAD SALMAN <MUHAMMADSALMAN7@carecloud.com>
2023-05-11 18:45:45 +0500

    Fixation

--- Fox.Database/StoredProcedure/FOX_PROC_GET_WORK_QUEUE_DETAILS_LIST.sql
+++ Fox.Database/StoredProcedure/FOX_PROC_GET_WORK_QUEUE_DETAILS_LIST.sql
@@ -1,7 +1,7 @@
 -- AUTHOR:  <MUHAMMAD SALMAN>                                                                                              
 -- CREATE DATE: <CREATE DATE, 05/01/2023>                                                                                              
 -- DESCRIPTION: <GET LIST OF WORK QUEUE DETAILS> 
-=============================================           
+---=============================================           
 -- FOX_PROC_GET_WORK_QUEUE_DETAILS_LIST '816631', 1012714                                                                                                                                                                              
 CREATE PROCEDURE [dbo].[FOX_PROC_GET_WORK_QUEUE_DETAILS_LIST]                                                                                                           
 (                                                                                                             
