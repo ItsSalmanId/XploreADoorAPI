e1a1cd82ede1c4ef81998b6892e2bfa9bd56e9d0
aftabkhan <aftabkhan@carecloud.com>
2023-09-20 15:32:15 +0500

    MFA token services V2 API BY aftab

--- FOX.BusinessOperations/Security/TokenService.cs
+++ FOX.BusinessOperations/Security/TokenService.cs
@@ -127,7 +127,7 @@ namespace FOX.BusinessOperations.Security
                 var userNameToken = new SqlParameter("@USERNAME", SqlDbType.BigInt) { Value = UserDetailsAuth.userID };
                 var userToken = new SqlParameter("@TOKEN", SqlDbType.VarChar) { Value = token };
                 var userProfile = new SqlParameter("@USER_PROFILE", SqlDbType.VarChar) { Value = JsonConvert.SerializeObject(UserDetailsAuth).ToString() };
-                var tokenModel = SpRepository<ProfileToken>.GetSingleObjectWithStoreProcedure(@"exec FOX_PROC_UPDATE_TOKEN @ISLOGOUT, @ISVALIDATE, @ISMFAVERIFIED, @USERNAME, @TOKEN , @USER_PROFILE", islogout, isUserValidate, isMFAVerify, userNameToken, userToken, userProfile);
+                var tokenModel = SpRepository<ProfileToken>.GetSingleObjectWithStoreProcedure(@"exec FOX_PROC_UPDATE_TOKEN @ISLOGOUT,@ISVALIDATE,@ISMFAVERIFIED,@USERNAME,@TOKEN ,@USER_PROFILE", islogout, isUserValidate, isMFAVerify, userNameToken, userToken, userProfile);  
 
                 return tokenModel;
             }
